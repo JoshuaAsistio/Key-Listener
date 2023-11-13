e8792655fe775052b903780b5ce04b10ec7e40a7
Elijah Joshua Asistio <salazarelijahjoshuaasistio@gmail.com>
2023-11-13 10:40:04 -1200

    Initial commit

---
+++ src/MyFrame.java
@@ -0,0 +1,74 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MyFrame extends JFrame implements KeyListener {
+	JLabel label;
+	
+	MyFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(null);
+		setSize(420, 420);
+		addKeyListener(this);
+		
+		label = new JLabel();
+		label.setBounds(0, 0, 100, 100);
+		label.setBackground(Color.red);
+		label.setOpaque(true);
+		
+		add(label);
+				
+		setVisible(true);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch(e.getKeyChar()) {
+		case 'a':
+			label.setLocation(label.getX() - 10, label.getY());
+			break;
+		case 'w':
+			label.setLocation(label.getX(), label.getY() - 10);
+			break;
+		case 'd':
+			label.setLocation(label.getX() + 10, label.getY());
+			break;
+		case 's':
+			label.setLocation(label.getX(), label.getY() + 10);
+			break;
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch(e.getKeyCode()) {
+		case 37:
+			label.setLocation(label.getX() - 10, label.getY());
+			break;
+		case 38:
+			label.setLocation(label.getX(), label.getY() - 10);
+			break;
+		case 39:
+			label.setLocation(label.getX() + 10, label.getY());
+			break;
+		case 40:
+			label.setLocation(label.getX(), label.getY() + 10);
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		//System.out.println("You released key character " + e.getKeyChar());
+		//System.out.println("You released key code " + e.getKeyCode());
+	}
+
+}
